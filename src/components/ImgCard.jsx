b461c48ddeefeaa8192924ed2f224dffb6224b16
rahim0004 <88199855+rahim0004@users.noreply.github.com>
2025-02-12 16:49:53 +0600

    initial

---
+++ src/components/ImgCard.jsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Link, useNavigate } from 'react-router';
+import axios from '../api';
+import { Rating } from '@mui/material';
+import { useAuth } from '../auth/AuthContext';
+
+
+export default function ImgMediaCard({ course, isDetail = false }) {
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    
+    const handleEnroll = async (courseId) => {
+        try {
+            const response = await axios.post(
+                `/api/courses/courses/${courseId}/enroll/`
+            );
+            if (isDetail) {
+                window.location.reload()
+            } else {
+                navigate(`/courses/${courseId}`)
+            }
+        } catch (err) {
+            console.error('Error fetching teachers:', err);
+        }
+    }
+
+    return (
+        <Card sx={{ maxWidth: 320 }}>
+            <CardMedia
+                component="img"
+                height="140"
+                image={course.banner ? course.banner : "/default.png"}
+                alt={course.title}
+            />
+            <CardContent>
+
+                <Typography gutterBottom variant="h6" component="div">
+                    {course.title.substring(0, 20)+". . ."}
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+
+                    {isDetail ? course.description : course.description.substring(0, 50) + ". . ."}
+                </Typography>
+                <Rating
+                    value={course.rating}
+                    readOnly
+                    precision={0.5}
+                />
+                <Typography variant="subtitle2" color="text.secondary" sx={{ mt: 1 }}>
+                    Instructor: {course.instructor.name}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                {window.location.pathname.includes("edit")?"":user?.email === course.instructor.email ? <Button
+                    size="small"
+                    variant='contained'
+                    color='warning'
+                    component={Link}
+                    to={`/courses/edit/${course.id}`}
+                >
+                    Edit
+                </Button> :
+                    course.has_enrolled ? "" :
+                        user ? <Button 
+                        color='secondary' 
+                        variant='contained' 
+                        size="small" 
+                        onClick={() => handleEnroll(course.id)}>Enroll</Button> :
+                        <small>
+                            Login to enroll.
+                        </small>
+                }
+                {
+                    isDetail ? "" :
+                        <Button
+                            size="small"
+                            variant='contained'
+                            component={Link}
+                            to={`/courses/${course.id}`}
+                        >
+                            View
+                        </Button>
+                }
+            </CardActions>
+        </Card>
+    );
+}
